3f02d301a868c20e796e6703f4619f4d7bdc4953
Mexson Fernandes <robomex2020@gmail.com>
2020-09-22 23:42:20 +0530

    load common utilies

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# ionic.utils
+# `ionic.utils`
+
+Get handy with common modules needed to build a ionic project.
+
+## Modules supported:
+* ToastController
+* 
+
+
+Note: Most of the code is taken from the documentation: https://ionicframework.com/docs/api
---
+++ utility.ts
@@ -0,0 +1,73 @@
+import { AlertController, LoadingController, ToastController } from "@ionic/angular";
+import { Router } from "@angular/router";
+import { LOCAL_STORAGE, WebStorageService } from "angular-webstorage-service";
+import { Inject } from "@angular/core";
+
+export class Utility {
+  constructor(
+    private toast: ToastController,
+    private alert: AlertController,
+    private router: Router,
+    @Inject(LOCAL_STORAGE) private storage: WebStorageService,
+    private loading: LoadingController,
+  ) {
+    this.storage;
+  }
+
+  async showLoading(message: string, duration=2000) {
+    const loading = await this.loading.create({
+      message: message,
+      duration: duration
+    });
+    await loading.present();
+  }
+
+  async showToast(message: string, duration = 2000, position = null) {
+    const toast = await this.toast.create({
+      message: message,
+      duration: duration,
+      position: position || "bottom",
+    });
+    toast.present();
+  }
+
+  async showAlert(header: string, message: string, buttonText = "Ok") {
+    const alert = await this.alert.create({
+      header: header,
+      message: message,
+      buttons: [buttonText],
+    });
+    await alert.present();
+  }
+
+  /*
+   ** Route app with array ['route', 'value']
+   */
+  navigateWithArray(arr: [any], param = {}) {
+    this.router.navigate(arr, param);
+  }
+
+  navigateWithUrl(url: string) {
+    this.router.navigateByUrl(url);
+  }
+
+  getStorageData(storage_key: string) {
+    return this.storage.get(JSON.parse(storage_key));
+  }
+
+  putStorageData(storage_key: string, data: any) {
+    this.storage.set(storage_key, JSON.stringify(data));
+  }
+
+  deleteStorageData(storage_key: string) {
+    this.storage.remove(storage_key);
+  }
+
+  getUTCTimestamp() {
+    return new Date().toISOString();
+  }
+
+  getCurrentLink() {
+    return window.location.pathname + window.location.search
+  }
+}
